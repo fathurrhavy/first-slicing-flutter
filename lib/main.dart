b57da99e6376cd81ba6e9c5c3470cede33847b88
Fathur Rhavy <fathurrhavy01@gmail.com>
2022-10-19 08:59:00 +0700

    my first slicing with flutter

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:day_1_bwa/pages/first_splash.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(SevenDays());
+
+class SevenDays extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstSplash(),
+    );
+  }
+}
